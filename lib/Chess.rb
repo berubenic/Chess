30bf5579717bf404ee273c1d71e4273ba5e1421d
Nicholas <berubenic@gmail.com>
2020-11-08 13:52:51 -0500

    small name changes to files

--- lib/Chess.rb
+++
@@ -1,6 +0,0 @@
-require "Chess/version"
-
-module Chess
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/chess.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'chess/version'
+
+# Chess
+module Chess
+end

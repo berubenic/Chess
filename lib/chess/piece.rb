2b029958293ff763f837c4cbeb1e605fde9ec4d6
Nicholas <berubenic@gmail.com>
2020-11-11 19:49:57 -0500

    Pawn possible_moves

--- lib/chess/piece.rb
+++ lib/chess/piece.rb
@@ -3,18 +3,38 @@
 module Chess
   # Pieces
   class Piece
-    attr_reader :color, :current_position
-    def initialize(color:, current_position:)
+    attr_reader :color, :current_position, :count
+    def initialize(color:, current_position:, count: 0)
       @color = color
       @current_position = current_position
+      @count = count
     end
 
     def update_position(coordinate)
       @current_position = coordinate
     end
+
+    private
+
+    def with_in_board?(coord)
+      coord.all? { |num| num >= 0 && num <= 7 }
+    end
   end
 
+  # Pawn
   class Pawn < Piece
+    private
+
+    def possible_moves(x, y)
+      possibilities = [
+        [x, y + 2],
+        [x, y + 1],
+        [x, y - 2],
+        [x, y - 1]
+      ]
+
+      possibilities.keep_if { |coord| with_in_board?(coord) }
+    end
   end
 
   class Rook < Piece

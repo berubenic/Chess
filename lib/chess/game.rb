6145f36d8b553f8a85f6c3aab8a62b7de0c20820
Nicholas <berubenic@gmail.com>
2020-11-12 18:47:30 -0500

    small Game script for initial setup

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -7,5 +7,14 @@ module Chess
       @player_one = player_one
       @player_two = player_two
     end
+
+    def setup_game
+      board.create_board
+      board.setup_board
+      player_one.input_name
+      player_two.input_name
+      player_one.assign_color('white')
+      player_two.assign_color('black')
+    end
   end
 end

159b5d6b201e15c421f7d2db7275521b5eec9f08
Nicholas <berubenic@gmail.com>
2020-11-21 17:33:05 -0500

    refactored en_passant methods to EnPassant module

--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -4,6 +4,7 @@ module Chess
   # Pawn
   class Pawn < Piece
     include SingleMovement
+    include EnPassant
 
     WHITE_MOVES = [
       -1,
@@ -70,41 +71,9 @@ module Chess
       @movements << additional_move if valid_move?(additional_move)
     end
 
-    def find_en_passant(result = [])
-      return unless en_passant_correct_row?
-
-      result << left_en_passant unless left_en_passant.nil?
-      result << right_en_passant unless right_en_passant.nil?
-      result
-    end
-
-    def left_en_passant
-      left_coordinate = [coordinate[0] - 1, coordinate[1]]
-      return nil unless tile_valid?(left_coordinate)
-
-      return [left_coordinate[0], left_coordinate[1] - 1] if color == 'white'
-      return [left_coordinate[0], left_coordinate[1] + 1] if color == 'black'
-    end
-
-    def right_en_passant
-      right_coordinate = [coordinate[0] + 1, coordinate[1]]
-      return nil unless tile_valid?(right_coordinate)
-
-      return [right_coordinate[0], right_coordinate[1] - 1] if color == 'white'
-      return [right_coordinate[0], right_coordinate[1] + 1] if color == 'black'
-    end
-
     def tile_valid?(coordinate)
       tile = board[coordinate[1]][coordinate[0]] if within_board?(coordinate)
       tile.class == Pawn && tile.two_squared
-    end
-
-    def en_passant_correct_row?
-      if color == 'white'
-        coordinate[1] == 3
-      elsif color == 'black'
-        coordinate[1] == 4
-      end
     end
   end
 end

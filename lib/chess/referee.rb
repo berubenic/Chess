638abbb080a736e535373009368ab66c07dc1d74
Nicholas <berubenic@gmail.com>
2020-11-22 10:24:44 -0500

    find_kings

--- lib/chess/referee.rb
+++ lib/chess/referee.rb
@@ -3,10 +3,12 @@
 module Chess
   # looks for check, checkmate and stalemate
   class Referee
-    attr_reader :board
+    attr_reader :board, :white_king, :black_king
 
     def initialize(board: nil)
       @board = board
+      @white_king = nil
+      @black_king = nil
     end
 
     def checkmate(king)
@@ -19,6 +21,21 @@ module Chess
         end
       end
       king.not_checkmate
+    end
+
+    def find_kings
+      board.each do |row|
+        row.each do |tile|
+          next if tile == ''
+
+          assign_kings(tile)
+        end
+      end
+    end
+
+    def assign_kings(tile)
+      @white_king = tile if tile.is_a?(King) && tile.color == 'white'
+      @black_king = tile if tile.is_a?(King) && tile.color == 'black'
     end
   end
 end
--- spec/referee_spec.rb
+++ spec/referee_spec.rb
@@ -56,5 +56,47 @@ module Chess
         end
       end
     end
+    describe '#find_kings' do
+      subject(:referee) { described_class.new }
+      let(:white_king) { instance_double(King, color: 'white', coordinate: [0, 7]) }
+      let(:black_king) { instance_double(King, color: 'black', coordinate: [0, 0]) }
+
+      before do
+        allow(white_king).to receive(:is_a?).with(King).and_return(true)
+        allow(black_king).to receive(:is_a?).with(King).and_return(true)
+      end
+
+      it 'assigns @white_king the white king' do
+        board = [
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          [white_king, '', '', '', '', '', '', '']
+        ]
+        referee.instance_variable_set(:@board, board)
+        referee.find_kings
+        expect(referee.white_king).to eq white_king
+      end
+
+      it 'assigns @black_king the black king' do
+        board = [
+          [black_king, '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', ''],
+          [white_king, '', '', '', '', '', '', '']
+        ]
+        referee.instance_variable_set(:@board, board)
+        referee.find_kings
+        expect(referee.black_king).to eq black_king
+      end
+    end
   end
 end

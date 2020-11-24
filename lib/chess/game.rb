6a9903b9c02e88c4b3445d78db00362d532b24f0
Nicholas <berubenic@gmail.com>
2020-11-23 19:58:13 -0500

    removed observer from Game

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-require 'observer'
-
 module Chess
   class Game
-    include Observable
-
     def initialize(board, referee)
       @board = board
       @referee = referee

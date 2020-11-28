bb0687950a6e9c05972ccf19d5a651dbcc221507
Nicholas <berubenic@gmail.com>
2020-11-28 11:15:49 -0500

    refactored rook tests

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -3,13 +3,12 @@
 # Knight spec
 
 module Chess
-  xdescribe Rook do
+  describe Rook do
     describe '#possible_movements' do
-      context 'Board is empty except for Rook' do
+      context 'rook is at [0, 0]' do
         subject(:rook) { described_class.new }
-
-        it 'assigns @movements an array of possible moves when at [0, 0]' do
-          board = [
+        let(:array) do
+          [
             [rook, '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -19,27 +18,21 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it 'assigns @movements an array of possible moves when at [0, 0]' do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [0, 0])
           rook.possible_movements
-          expect(rook.movements.include?([0, 1])).to be true
-          expect(rook.movements.include?([0, 2])).to be true
-          expect(rook.movements.include?([0, 3])).to be true
-          expect(rook.movements.include?([0, 4])).to be true
-          expect(rook.movements.include?([0, 5])).to be true
-          expect(rook.movements.include?([0, 6])).to be true
-          expect(rook.movements.include?([0, 7])).to be true
-          expect(rook.movements.include?([1, 0])).to be true
-          expect(rook.movements.include?([2, 0])).to be true
-          expect(rook.movements.include?([3, 0])).to be true
-          expect(rook.movements.include?([4, 0])).to be true
-          expect(rook.movements.include?([5, 0])).to be true
-          expect(rook.movements.include?([6, 0])).to be true
-          expect(rook.movements.include?([7, 0])).to be true
+          expect(rook.movements).to contain_exactly([0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [1, 0],
+                                                    [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0])
         end
-
-        it 'assigns @movements an array of possible moves when at [3, 3]' do
-          board = [
+      end
+      context 'rook is at [3, 3]' do
+        subject(:rook) { described_class.new }
+        let(:array) do
+          [
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -49,31 +42,22 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it 'assigns @movements an array of possible moves when at [3, 3]' do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [3, 3])
           rook.possible_movements
-          expect(rook.movements.include?([3, 0])).to be true
-          expect(rook.movements.include?([3, 1])).to be true
-          expect(rook.movements.include?([3, 2])).to be true
-          expect(rook.movements.include?([3, 4])).to be true
-          expect(rook.movements.include?([3, 5])).to be true
-          expect(rook.movements.include?([3, 6])).to be true
-          expect(rook.movements.include?([3, 7])).to be true
-          expect(rook.movements.include?([0, 3])).to be true
-          expect(rook.movements.include?([1, 3])).to be true
-          expect(rook.movements.include?([2, 3])).to be true
-          expect(rook.movements.include?([4, 3])).to be true
-          expect(rook.movements.include?([5, 3])).to be true
-          expect(rook.movements.include?([6, 3])).to be true
-          expect(rook.movements.include?([7, 3])).to be true
+          expect(rook.movements).to contain_exactly([3, 0], [3, 1], [3, 2], [3, 4], [3, 5], [3, 6], [3, 7], [0, 3],
+                                                    [1, 3], [2, 3], [4, 3], [5, 3], [6, 3], [7, 3])
         end
       end
       context "A friendly piece occupies one of it's possible moves" do
         subject(:rook) { described_class.new(color: 'white') }
         let(:friendly) { instance_double(Piece, color: 'white') }
-
-        it 'assigns @movements an array of possible moves when at [0, 0]' do
-          board = [
+        let(:array) do
+          [
             [rook, '', '', '', '', '', '', ''],
             [friendly, '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -83,32 +67,22 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it 'assigns @movements an array of possible moves when at [0, 0]' do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [0, 0])
           rook.possible_movements
-          expect(rook.movements.include?([1, 0])).to be true
-          expect(rook.movements.include?([2, 0])).to be true
-          expect(rook.movements.include?([3, 0])).to be true
-          expect(rook.movements.include?([4, 0])).to be true
-          expect(rook.movements.include?([5, 0])).to be true
-          expect(rook.movements.include?([6, 0])).to be true
-          expect(rook.movements.include?([7, 0])).to be true
-          expect(rook.movements.include?([0, 1])).to be false
-          expect(rook.movements.include?([0, 2])).to be false
-          expect(rook.movements.include?([0, 3])).to be false
-          expect(rook.movements.include?([0, 4])).to be false
-          expect(rook.movements.include?([0, 5])).to be false
-          expect(rook.movements.include?([0, 6])).to be false
-          expect(rook.movements.include?([0, 7])).to be false
+          expect(rook.movements).to contain_exactly([1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0])
         end
       end
       context "Multiple friendly pieces occupying it's possible moves" do
         subject(:rook) { described_class.new(color: 'black') }
         let(:friendly_1) { instance_double(Piece, color: 'black') }
         let(:friendly_2) { instance_double(Piece, color: 'black') }
-
-        it 'assigns @movements an array of possible moves when at [4, 4]' do
-          board = [
+        let(:array) do
+          [
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -118,32 +92,22 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it 'assigns @movements an array of possible moves when at [4, 4]' do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [4, 4])
           rook.possible_movements
-          expect(rook.movements.include?([4, 0])).to be true
-          expect(rook.movements.include?([4, 1])).to be true
-          expect(rook.movements.include?([4, 2])).to be true
-          expect(rook.movements.include?([4, 3])).to be true
-          expect(rook.movements.include?([5, 4])).to be true
-          expect(rook.movements.include?([6, 4])).to be true
-          expect(rook.movements.include?([7, 4])).to be true
-          expect(rook.movements.include?([4, 5])).to be false
-          expect(rook.movements.include?([4, 6])).to be false
-          expect(rook.movements.include?([4, 7])).to be false
-          expect(rook.movements.include?([3, 4])).to be false
-          expect(rook.movements.include?([2, 4])).to be false
-          expect(rook.movements.include?([1, 4])).to be false
-          expect(rook.movements.include?([0, 4])).to be false
+          expect(rook.movements).to contain_exactly([4, 0], [4, 1], [4, 2], [4, 3], [5, 4], [6, 4], [7, 4])
         end
       end
       context "Enemy pieces don't matter" do
         subject(:rook) { described_class.new(color: 'black') }
         let(:enemy_1) { instance_double(Piece, color: 'white') }
         let(:enemy_2) { instance_double(Piece, color: 'white') }
-
-        it 'assigns @movements an array of possible moves when at [4, 4]' do
-          board = [
+        let(:array) do
+          [
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -153,32 +117,22 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it 'assigns @movements an array of possible moves when at [4, 4]' do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [4, 4])
           rook.possible_movements
-          expect(rook.movements.include?([4, 0])).to be true
-          expect(rook.movements.include?([4, 1])).to be true
-          expect(rook.movements.include?([4, 2])).to be true
-          expect(rook.movements.include?([4, 3])).to be true
-          expect(rook.movements.include?([5, 4])).to be true
-          expect(rook.movements.include?([6, 4])).to be true
-          expect(rook.movements.include?([7, 4])).to be true
-          expect(rook.movements.include?([4, 5])).to be false
-          expect(rook.movements.include?([4, 6])).to be false
-          expect(rook.movements.include?([4, 7])).to be false
-          expect(rook.movements.include?([3, 4])).to be false
-          expect(rook.movements.include?([2, 4])).to be false
-          expect(rook.movements.include?([1, 4])).to be false
-          expect(rook.movements.include?([0, 4])).to be false
+          expect(rook.movements).to contain_exactly([4, 0], [4, 1], [4, 2], [4, 3], [5, 4], [6, 4], [7, 4])
         end
       end
     end
     describe '#possible_captures' do
       context 'Board is empty except for Rook' do
         subject(:rook) { described_class.new }
-
-        it 'assigns @captures an empty array' do
-          board = [
+        let(:array) do
+          [
             [rook, '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -188,6 +142,10 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it 'assigns @captures an empty array' do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [0, 0])
           rook.possible_captures
@@ -197,9 +155,8 @@ module Chess
       context 'A enemy piece is in range' do
         subject(:rook) { described_class.new(color: 'white') }
         let(:enemy) { instance_double(Piece, color: 'black') }
-
-        it "assigns @captures an array with it's possible capture" do
-          board = [
+        let(:array) do
+          [
             [rook, '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             [enemy, '', '', '', '', '', '', ''],
@@ -209,19 +166,22 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it "assigns @captures an array with it's possible capture" do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [0, 0])
           rook.possible_captures
-          expect(rook.captures).to eq([[0, 2]])
+          expect(rook.captures).to contain_exactly([0, 2])
         end
       end
       context 'A enemy and friendly piece is in range' do
         subject(:rook) { described_class.new(color: 'white') }
         let(:friendly) { instance_double(Piece, color: 'white') }
         let(:enemy) { instance_double(Piece, color: 'black') }
-
-        it "assigns @captures an array with it's possible capture" do
-          board = [
+        let(:array) do
+          [
             [rook, '', '', friendly, '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -231,19 +191,22 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it "assigns @captures an array with it's possible capture" do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [0, 0])
           rook.possible_captures
-          expect(rook.captures).to eq([[0, 5]])
+          expect(rook.captures).to contain_exactly([0, 5])
         end
       end
       context 'Two enemy pieces in range' do
         subject(:rook) { described_class.new(color: 'white') }
         let(:enemy_1) { instance_double(Piece, color: 'black') }
         let(:enemy_2) { instance_double(Piece, color: 'black') }
-
-        it "assigns @captures an array with it's possible captures" do
-          board = [
+        let(:array) do
+          [
             [rook, enemy_1, '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '', ''],
@@ -253,11 +216,40 @@ module Chess
             ['', '', '', '', '', '', '', ''],
             [enemy_2, '', '', '', '', '', '', '']
           ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it "assigns @captures an array with it's possible captures" do
           rook.instance_variable_set(:@board, board)
           rook.instance_variable_set(:@coordinate, [0, 0])
           rook.possible_captures
-          expect(rook.captures.include?([1, 0])).to be true
-          expect(rook.captures.include?([0, 7])).to be true
+          expect(rook.captures).to contain_exactly([1, 0], [0, 7])
+        end
+      end
+      context 'Two enemy pieces in range, a friendly blocks one' do
+        subject(:rook) { described_class.new(color: 'white') }
+        let(:friendly) { instance_double(Piece, color: 'white') }
+        let(:enemy_1) { instance_double(Piece, color: 'black') }
+        let(:enemy_2) { instance_double(Piece, color: 'black') }
+        let(:array) do
+          [
+            [rook, enemy_1, '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            [friendly, '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            [enemy_2, '', '', '', '', '', '', '']
+          ]
+        end
+        let(:board) { instance_double(Board, board: array) }
+
+        it "assigns @captures an array with it's possible captures" do
+          rook.instance_variable_set(:@board, board)
+          rook.instance_variable_set(:@coordinate, [0, 0])
+          rook.possible_captures
+          expect(rook.captures).to contain_exactly([1, 0])
         end
       end
     end

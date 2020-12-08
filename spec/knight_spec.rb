f07c58815e5b18fc652e2f9ecddbdd6606bbc891
Nicholas <berubenic@gmail.com>
2020-12-08 18:21:07 -0500

    refactored Knight#all_possible_moves and #all_possible_captures

---
+++ spec/knight_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Knight specs
+module Chess
+  describe Knight do
+    subject(:knight) { described_class.new(x_coordinate: 4, y_coordinate: 4, color: 'white', content: 'Kn') }
+
+    describe '#all_possible_movements' do
+      it 'returns result' do
+        expect(knight.all_possible_movements).to contain_exactly([5, 2], [6, 3], [6, 5], [5, 6], [3, 6], [2, 5], [2, 3], [3, 2])
+      end
+    end
+    describe '#all_possible_captures' do
+      it 'returns result' do
+        expect(knight.all_possible_captures).to contain_exactly([5, 2], [6, 3], [6, 5], [5, 6], [3, 6], [2, 5], [2, 3], [3, 2])
+      end
+    end
+  end
+end

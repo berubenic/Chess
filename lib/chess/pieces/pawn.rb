2704ae10f03817767449d9512aae5e7008e9de9e
Nicholas <berubenic@gmail.com>
2020-12-08 18:07:19 -0500

    Pawn#possible_moves and #possible_captures

--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -17,28 +17,41 @@ module Chess
       raise NotImplementedError
     end
 
-    def first_possible_move(direction, result = [])
-      move = [current_coordinate[0], current_coordinate[1] + direction[0]]
-      result << move
-      return result if current_coordinate == starting_coordinate
+    def all_possible_movements(directions, result = [])
+      result = first_possible_move(directions[0], result)
+      return result unless current_coordinate == starting_coordinate
 
-      second_possible_move(direction, result)
+      second_possible_move(directions[1], result)
     end
 
-    def second_possible_move(direction, result)
-      additional_move = [current_coordinate[0], current_coordinate[1] + direction[1]]
+    def first_possible_move(direction, result = [])
+      move = [current_coordinate[0], current_coordinate[1] + direction]
+      result << move
+      result
+    end
+
+    def second_possible_move(direction, result = [])
+      additional_move = [current_coordinate[0], current_coordinate[1] + direction]
       result << additional_move
+      result
+    end
+
+    def all_possible_captures(directions, result = [])
+      directions.each do |x_coordinate, y_coordinate|
+        capture = [current_coordinate[0] + x_coordinate, current_coordinate[1] + y_coordinate]
+        result << capture
+      end
       result
     end
   end
   # WhitePawn piece
   class WhitePawn < Pawn
-    WHITE_MOVES = [
+    MOVE_DIRECTIONS = [
       -1,
       -2
     ].freeze
 
-    WHITE_CAPTURES = [
+    CAPTURE_DIRECTIONS = [
       [1, -1],
       [-1, -1]
     ].freeze
@@ -55,18 +68,22 @@ module Chess
       6
     end
 
-    def possible_movements
-      first_possible_move(WHITE_MOVES)
+    def all_possible_movements(directions = MOVE_DIRECTIONS)
+      super
+    end
+
+    def all_possible_captures(directions = CAPTURE_DIRECTIONS)
+      super
     end
   end
   # BlackPawn piece
   class BlackPawn < Pawn
-    BLACK_MOVES = [
+    MOVE_DIRECTIONS = [
       1,
       2
     ].freeze
 
-    BLACK_CAPTURES = [
+    CAPTURE_DIRECTIONS = [
       [1, 1],
       [-1, 1]
     ].freeze
@@ -81,6 +98,14 @@ module Chess
 
     def default_y_coordinate
       1
+    end
+
+    def all_possible_movements(directions = MOVE_DIRECTIONS)
+      super
+    end
+
+    def all_possible_captures(directions = CAPTURE_DIRECTIONS)
+      super
     end
   end
 end
